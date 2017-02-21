b3cb705d3368f1f46e7cfd6f9aec13fce95da494
3ll3n <ellengrafton01@gmail.com>
2017-02-21 14:18:26 +0000

    Added constructor Customer/Atm

--- Atm.java
+++ Atm.java
@@ -1,3 +1,12 @@
 public class Atm {
-  
+
+  private String location;
+  private int money;
+  private int limit;
+
+  public Atm(String location, int money, int limit){
+    this.location = location;
+    this.money = money;
+    this.limit = limit;
+  } 
 }
--- Customer.java
+++ Customer.java
@@ -1,3 +1,13 @@
 public class Customer {
-  
+
+  private String name;
+  private int age;
+  private int wallet;
+
+  public Customer(String name, int age, int wallet){
+    this.name = name;
+    this.age = age;
+    this.wallet = wallet;
+  }
+
 }
